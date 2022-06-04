87fb7ee1fe1e0ee83ed730d046bdcaa962bd2baa
Christopher Oswald <chrisozzynp@gmail.com>
2022-06-04 16:08:50 -0700

    Add Account & Data services

---
+++ Services/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using Milestone_cst_350.Models;
+
+namespace Milestone_cst_350.Services
+{
+    public class AccountService : DataService
+    {
+        public AccountService()
+        {
+            // ...
+        }
+
+        // TODO: RegisterUser
+        public bool RegisterUser(UserModel user)
+        {
+            return false;
+        }
+
+        // TODO: AuthenticateUser
+        public bool AuthenticateUser(UserModel user)
+        {
+            return false;
+        }
+    }
+}
---
+++ Services/DataService.cs
@@ -0,0 +1,13 @@
+﻿namespace Milestone_cst_350.Services
+{
+    // If there is a way to do this with an interface I would do that but idk how you'd inherit the values...
+
+    public class DataService
+    {
+        /// <summary>
+        /// This should make it easy to maintain a single value across all our data services.
+        /// The protected keyword should also allow classes deriving this value access to it.
+        /// </summary>
+        protected string ConnectionString { get { return @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=minesweeper;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; } }
+    }
+}
