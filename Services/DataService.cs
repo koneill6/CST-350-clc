880f744cd226ea4e38e3c052ec97c8709c6fbec3
koneill6 <79379717+koneill6@users.noreply.github.com>
2022-07-14 12:50:56 -0500

    Update DataService.cs

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -8,6 +8,6 @@
         /// This should make it easy to maintain a single value across all our data services.
         /// The protected keyword should also allow classes deriving this value access to it.
         /// </summary>
-        protected string ConnectionString { get { return @"Server=tcp:tempgcu.database.windows.net,1433;Initial Catalog=minesweeper;Persist Security Info=False;User ID=clc.student;Password=GCU@access1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; } }
+        protected string ConnectionString { get { return @"Server=tcp:tempgcu.database.windows.net,1433;Initial Catalog=minesweeper;Persist Security Info=False;User ID=clc.student;Password='GCU@access1';MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; } }
     }
 }
