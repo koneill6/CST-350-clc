f8245dbee907efbceef7a0cafed5f6714e9c15da
Joel Smith <joelsmith.2019@gmail.com>
2022-07-14 10:23:32 -0700

    Update Connection String

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -8,6 +8,6 @@
         /// This should make it easy to maintain a single value across all our data services.
         /// The protected keyword should also allow classes deriving this value access to it.
         /// </summary>
-        protected string ConnectionString { get { return @"Database=minesweeper;Data Source=127.0.0.1;User Id=azure;Password=6#vWHD_$"; } }
+        protected string ConnectionString { get { return @"Server=tcp:tempgcu.database.windows.net,1433;Initial Catalog=minesweeper;Persist Security Info=False;User ID=clc.student;Password=GCU@access1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; } }
     }
 }
